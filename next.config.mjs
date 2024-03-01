4c914582a0a283cffdb7f160b2c1531f7875b679
alexamoros <alejandroemail1@gmail.com>
2024-03-01 18:54:41 +0100

    Update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ import "./env.mjs"
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["@prisma/client"],
-  },
 }
 
 export default nextConfig
