64289e744d8a88b00c0dd66742b85b6a79c23427
alexamoros <alejandroemail1@gmail.com>
2024-03-21 19:45:50 +0100

    Favicon for password website logic added

--- components/Dashboard/Table/password-listing.tsx
+++ components/Dashboard/Table/password-listing.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Suspense, useEffect, useState } from "react"
 import Link from "next/link"
 import { PasswordTableType } from "@/types"
 import { useTranslations } from "next-intl"
@@ -7,6 +8,8 @@ import { useTranslations } from "next-intl"
 import { PasswordActions } from "@/components/Dashboard/Table/password-actions"
 
 import "moment/locale/es"
+import Image from "next/image"
+import { getFavicon } from "@/actions/get-favicon"
 import moment from "moment"
 
 type props = {
@@ -15,22 +18,63 @@ type props = {
 }
 
 export function Password({ password, locale }: props) {
+  const [favicon, setFavicon] = useState(null)
+  const [loading, setLoading] = useState(true)
   const t = useTranslations("dashboard")
   moment.locale(locale)
   const time = moment(new Date(password.updatedAt)).fromNow()
+
+  useEffect(() => {
+    if (password.website) {
+      getFavicon({ query: { url: password.website } })
+        .then((res) => {
+          setFavicon(res)
+          setLoading(false)
+        })
+        .catch((err) => console.log(err))
+    }
+  }, [password.website])
+
+  const getHostname = (url) => {
+    const domain = new URL(url).hostname
+    return domain.replace("www.", "")
+  }
+
+  const getInitials = (url) => {
+    let hostname = getHostname(url)
+    return hostname.charAt(0).toUpperCase()
+  }
+
   return (
     <div className="flex items-center justify-between p-4">
-      <div className="grid gap-1">
-        <Link
-          href={`/dashboard/save/decrypt/${password.id}`}
-          className="font-semibold hover:underline"
-        >
-          {password.username}
-        </Link>
-        <div>
-          <p className="text-sm text-muted-foreground">
-            {t("updated")} {time}
-          </p>
+      <div className="flex items-start gap-4">
+        {favicon ? (
+          <Image
+            src={favicon}
+            priority
+            loading="eager"
+            alt={`${getHostname(password.website)} favicon`}
+            width="24"
+            height="24"
+            className="size-8 self-center rounded border-2 border-solid p-1"
+          />
+        ) : (
+          <div className="flex size-8 items-center justify-center self-center rounded border-2 border-solid p-1">
+            <p>{getInitials(password.website)}</p>
+          </div>
+        )}
+        <div className="grid gap-1">
+          <Link
+            href={`/dashboard/save/decrypt/${password.id}`}
+            className="font-semibold hover:underline"
+          >
+            {getHostname(password.website)}
+          </Link>
+          <div>
+            <p className="text-sm text-muted-foreground">
+              {t("updated")} {time}
+            </p>
+          </div>
         </div>
       </div>
       <PasswordActions id={password.id} username={password.username} />
--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,14 @@ const withNextIntl = createNextIntlPlugin()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // This wildcard allows all domains
+      },
+    ],
+  },
 }
 
 export default withNextIntl(nextConfig)
